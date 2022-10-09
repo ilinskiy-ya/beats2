ff4d5985ed5c4038e39e27d6a4d6cdae41a09ff7
ilinskiy <ilinskiy.ya@gmail.com>
2022-10-09 14:12:07 +0300

    master

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,23 @@ closePopUp.addEventListener('click',() => {
     popUp.classList.remove('active');
 })
 
+
+//rewiews
+
+const findBlockByAlias = (alias) => {
+    return $(".reviews__display-inner").filter((ndx, item)=>{
+        return $(item).attr("data-linked-with") == alias
+    } );
+};
+
+$(".interectiv-avatar-link").click((e)=> {
+    e.preventDefault();    
+
+    const $this = $(e.currentTarget);
+    const target= $this.attr ("data-open");
+    const itemToShow = findBlockByAlias(target);
+    const curItem = $this.closest(".interectiv-avatar");
+
+    itemToShow.addClass("active").siblings().removeClass("active");
+    curItem.addClass('active').siblings().removeClass('active');
+});
